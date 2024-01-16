53235e151845705e152aa757728ec1674537594a
Milan Prohorenia <nightmare-dev@mail.ru>
2024-01-16 04:31:35 +0300

    Rewritten with good structure
    Added blueprints

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,34 +1,37 @@
 from datetime import datetime
 from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import current_app
+from flaskblog import db, login_manager
 from flask_login import UserMixin
-from flaskblog import app, db, login_manager
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
-    posts = db.relationship("Post", backref="author", lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self):
-        s = Serializer(app.config["SECRET_KEY"])
-        return s.dumps({"user_id": self.id})
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
 
     @staticmethod
     def verify_reset_token(token, max_age=300):
-        s = Serializer(app.config["SECRET_KEY"])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token, max_age=max_age)["user_id"]
+            user_id = s.loads(token, max_age=max_age)['user_id']
         except:
             return None
         return User.query.get(user_id)
 
-    def __rept__(self):
+    def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
@@ -37,7 +40,7 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __rept__(self):
-        return f"User('{self.title}', '{self.date_posted}')"
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
---
+++ flaskblog/users/utils.py
@@ -0,0 +1,40 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for, current_app
+from flask_mail import Message
+from flaskblog import mail
+
+
+def update_picture(form_picture, old_filename):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    print(f'print1 {form_picture.filename}')
+    print(f'print2 {old_filename}')
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, "static//profile_pics", picture_filename)
+
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+
+    if old_filename != "default.jpg":
+        picture_path = os.path.join(current_app.root_path, "static//profile_pics", old_filename)
+        if os.path.exists(picture_path):
+            os.remove(picture_path)
+
+    return picture_filename
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@test.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('users.reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
